ed7e9aa882e9bb98d4d30aa13eb463498f1756c2
Andrei Mateescu <andrei@amateescu.me>
2013-02-27 11:09:40 +0100

    Issue #1894772 by amateescu, alberto56: Fixed hook_menu_attribute_info() API example.

--- menu_attributes.api.php
+++ menu_attributes.api.php
@@ -6,9 +6,9 @@
  */
 
 /**
- * Alter the list of menu item attributes.
+ * Inform the menu_attributes module about custom attributes.
  *
- * @param $attributes
+ * @return
  *   An array of attributes to be controlled by Menu Attributes, keyed by
  *   attribute name. Each attribute record should be an array with the following
  *   key/value pairs:
@@ -20,7 +20,7 @@
  * @see menu_attributes_menu_attribute_info()
  * @see menu_attributes_get_menu_attribute_info()
  */
-function hook_menu_attribute_info(array &$attributes) {
+function hook_menu_attribute_info() {
   // Add a Tabindex attribute.
   $info['tabindex'] = array(
     'label' => t('Tabindex'),
@@ -31,6 +31,20 @@ function hook_menu_attribute_info(array &$attributes) {
     ),
   );
 
+  return $info;
+}
+
+/**
+ * Alter the list of menu item attributes.
+ *
+ * @param $attributes
+ *   An array of attributes to be controlled by Menu Attributes, keyed by
+ *   attribute name.
+ *
+ * @see hook_menu_attribute_info()
+ * @see menu_attributes_get_menu_attribute_info()
+ */
+function hook_menu_attribute_info_alter(array &$attributes) {
   // Remove the Access Key attribute.
   unset($attributes['accesskey']);
 }
